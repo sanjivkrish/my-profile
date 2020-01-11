7fc12eb4ca9d3f1d3198db80c8cc0b6d51d7c4c1
sanjivkrish <sanjiv436.k@gmail.com>
2020-01-11 22:04:17 +0100

    Change toolbar img

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`
+    title: `Sanjiv Krishnasamy`,
+    description: `Personal profile`,
+    author: `@sanjiv_krishnasamy`
   },
   plugins: [
     `gatsby-theme-material-ui`,
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/letter-s.png` // This path is relative to the root of the site.
+        icon: `src/images/letter-s.jpg` // This path is relative to the root of the site.
       }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
